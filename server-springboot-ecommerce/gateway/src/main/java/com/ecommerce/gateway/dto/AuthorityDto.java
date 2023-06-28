c1344ab95573f2f70ddf0f267426b5a8b0ac3cd4
Nck Arachchci <76784581+nckarachchi@users.noreply.github.com>
2023-06-28 19:37:31 +0530

    full api gateway

---
+++ server-springboot-ecommerce/gateway/src/main/java/com/ecommerce/gateway/dto/AuthorityDto.java
@@ -0,0 +1,15 @@
+package com.ecommerce.gateway.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Setter
+public class AuthorityDto {
+    private String authority;
+}
---
+++ server-springboot-ecommerce/gateway/src/main/java/com/ecommerce/gateway/dto/ErrorDto.java
@@ -0,0 +1,14 @@
+package com.ecommerce.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class ErrorDto {
+    private String error_message;
+}
